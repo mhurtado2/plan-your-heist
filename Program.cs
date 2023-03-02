5c1148c0613185c7699a52e8e699e128ede8cec9
Shane Hurtado <mhurtado1@berklee.edu>
2023-03-02 16:03:03 -0600

    phase 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace planYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamMember CreateNewTeamMember()
+            {
+            //console write plan your hesit
+            Console.WriteLine("Plan Your Heist");
+            Console.WriteLine("What is your name: ");
+             string Username= Console.ReadLine().Trim();
+            Console.WriteLine("What is your skill level: ");
+             string SkillLevel = Console.ReadLine().Trim();
+             int numAnswer;
+             bool isNumber = int.TryParse(SkillLevel, out numAnswer);
+              Console.WriteLine("What is your courage level: ");
+             string CourageLevel = Console.ReadLine().Trim();
+              double numAnswerTwo;
+             bool isNumberTwo = double.TryParse(CourageLevel, out numAnswerTwo);
+             
+            
+            // TeamMember teamMember = new TeamMember(Username, numAnswer, numAnswerTwo);
+
+            //create instance of team member
+
+       
+            
+         TeamMember teamMember = new TeamMember(Username, numAnswer, numAnswerTwo);
+
+        Console.WriteLine(teamMember.GetDescription());
+                
+            return teamMember;  
+        }
+        CreateNewTeamMember();   
+    }
+  }
+}
---
+++ Team.cs
@@ -0,0 +1,23 @@
+using System; 
+
+namespace planYourHeist {
+    public class TeamMember {
+        public string Name {get; set;} //make sure to make these public so rest of app can access them
+
+        public int Level {get; set;}
+
+        public double Courage {get; set;}
+
+        public TeamMember(string name, int level, double courage ){
+            Name = name;
+            Level = level;
+            Courage = courage;
+        }
+
+        //get description type method from adventurer thing
+
+         public string GetDescription(){
+       return $"Team Member: {Name} has a skill level of: {Level} and a courage level of: {Courage}";
+    }
+    }
+}
